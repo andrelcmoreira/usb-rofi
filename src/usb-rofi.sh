9b75507d981fcdc27cdf581cb31002cb73b9de3e
André L. C. Moreira <alcm99@gmail.com>
2022-01-01 15:35:46 -0300

    adding 'set_variables' function to initialize the script variables

--- src/usb-rofi.sh
+++ src/usb-rofi.sh
@@ -1,47 +1,13 @@
 #!/bin/sh
 
-APP="usb-rofi"
-ICON="/usr/share/usb-rofi/usb-icon.png"
-USER=USERNAME
-
 export DISPLAY=:0
-
-do_umount() {
-  daemon_init=$(ps -p 1 -o comm=)
-
-  if [ "$daemon_init" = "systemd" ]; then
-    systemd-umount "$1"
-  else
-    umount "$1"
-  fi
-}
-
-do_mount() {
-  daemon_init=$(ps -p 1 -o comm=)
-
-  if [ "$daemon_init" = "systemd" ]; then
-    systemd-mount --no-block --automount=yes --collect "$1" "$2"
-  else
-    mount "$1" "$2"
-  fi
-}
-
-clean_mp() {
-  mount_point="$1"
-
-  if [ -s "$mount_point" ]; then
-    if do_umount "$mount_point" ; then
-      rmdir "$mount_point"
-    fi
-  fi
-}
 
 umount_device() {
   selected_row=$(df -h | rofi -dmenu -p "Select the device to umount")
   mount_point=$(echo "$selected_row" | awk '{print $6}')
 
   if [ -s "$mount_point" ]; then
-    if do_umount "$mount_point" ; then
+    if eval "$UMOUNT $mount_point" ; then
       rmdir "$mount_point"
       notify_cmd="notify-send \"$APP\" \"Device umounted, now you can remove it safely\" -i \"$ICON\""
       su -c "$notify_cmd" "$USER"
@@ -57,20 +23,51 @@ mount_device() {
     mount_point="/mnt/$2"
 
     mkdir "$mount_point"
-    if do_mount "$1" "$mount_point" ; then
+    if eval "$MOUNT $1 $mount_point" ; then
       notify_cmd="notify-send \"$APP\" \"Device $1 mounted at $mount_point\" -i \"$ICON\""
       su -c "$notify_cmd" "$USER"
     fi
   fi
 }
 
+clean_mp() {
+  mount_point="$1"
+
+  if [ -s "$mount_point" ]; then
+    if eval "$UMOUNT $mount_point" ; then
+      rmdir "$mount_point"
+    fi
+  fi
+}
+
+set_variables() {
+  pid_1=$(cat /proc/1/comm)
+
+  case $pid_1 in
+    # TODO: put a comment here explaining the difference between systemd-mount and mount
+    systemd)
+      MOUNT="systemd-mount --no-block --automount=yes --collect"
+      UMOUNT="systemd-umount" ;;
+    *)
+      MOUNT="mount"
+      UMOUNT="umount" ;;
+  esac
+
+  APP="usb-rofi"
+  ICON="/usr/share/$APP/usb-icon.png"
+  USER=USERNAME
+}
+
 main() {
+  set_variables
+
   while getopts 'd:c:u' op ; do
     case $op in
-      d) dev_part=$(echo "$OPTARG" | cut -f1 -d":")
-         dev_desc=$(echo "$OPTARG" | cut -f2 -d":")
+      d)
+        dev_part=$(echo "$OPTARG" | cut -f1 -d":")
+        dev_desc=$(echo "$OPTARG" | cut -f2 -d":")
 
-         mount_device "$dev_part" "$dev_desc" ;;
+        mount_device "$dev_part" "$dev_desc" ;;
       c) clean_mp "$OPTARG" ;;
       u) umount_device ;;
       *) ;;
